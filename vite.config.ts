af3456369d1fa6e3e46f56475f6f26240ef4c1f7
alkariin <alkariin81@gmail.com>
2026-06-28 18:15:13 +0200

    chore: add tailwind v4 as build dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import { VitePWA } from 'vite-plugin-pwa';
+  import tailwindcss from '@tailwindcss/vite';
   import path from 'path';
 
   export default defineConfig({
     plugins: [
+      tailwindcss(),
       react(),
       VitePWA({
         registerType: 'autoUpdate',
